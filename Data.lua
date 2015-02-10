6b9c54bf5a6aa9ac602cce95d78af1573b272f01
Elad Hoffer <elad.hoffer@gmail.com>
2015-02-10 17:36:48 +0200

    CIFAR first commit

---
+++ Data.lua
@@ -0,0 +1,108 @@
+require 'eladtools'
+local opt = opt or {}
+local Dataset = opt.dataset or 'Cifar10'
+local PreProcDir = opt.preProcDir or './'
+local Whiten = opt.whiten or false
+local DataPath = opt.datapath or '/home/ehoffer/Datasets/'
+
+local TestData
+local TrainData
+local Classes
+
+if Dataset =='Cifar100' then
+    TrainData = torch.load(DataPath .. 'Cifar100/cifar100-train.t7')
+    TestData = torch.load(DataPath .. 'Cifar100/cifar100-test.t7')
+    TrainData.labelCoarse:add(1)
+    TestData.labelCoarse:add(1)
+    Classes = torch.linspace(1,100,100):storage():totable()
+elseif Dataset == 'Cifar10' then
+    TrainData = torch.load(DataPath .. 'Cifar10/cifar10-train.t7')
+    TestData = torch.load(DataPath .. 'Cifar10/cifar10-test.t7')
+    Classes = {'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
+end
+--Preprocesss
+TrainData.label:add(1)
+TestData.label:add(1)
+TrainData.data = TrainData.data:float()
+TestData.data = TestData.data:float()
+
+
+--Preprocesss
+local meansfile = paths.concat(PreProcDir,'means.t7')
+if Whiten then
+    require 'unsup'
+    local means, P, invP
+    local Pfile = paths.concat(PreProcDir,'P.t7')
+    local invPfile = paths.concat(PreProcDir,'invP.t7')
+
+    if (paths.filep(Pfile) and paths.filep(invPfile) and paths.filep(meansfile)) then
+        P = torch.load(Pfile)
+        invP = torch.load(invPfile)
+        means = torch.load(meansfile)
+        TrainData.data = unsup.zca_whiten(TrainData.data:double(), means, P, invP)
+    else
+        TrainData.data, means, P, invP = unsup.zca_whiten(TrainData.data:double())
+        torch.save(Pfile,P)
+        torch.save(invPfile,invP)
+        torch.save(meansfile,means)
+    end
+    TestData.data = unsup.zca_whiten(TestData.data:double(), means, P, invP)
+   
+
+    TrainData.data = TrainData.data:float()
+    TestData.data = TestData.data:float()
+
+else
+    local means, std
+    local loaded = false
+    local stdfile = paths.concat(PreProcDir,'std.t7')
+    if paths.filep(meansfile) and paths.filep(stdfile) then
+        means = torch.load(meansfile)
+        std = torch.load(stdfile)
+        loaded = true
+    end
+    if not loaded then
+        means = torch.mean(TrainData.data, 1):squeeze()
+    end
+    TrainData.data:add(-1, means:view(1,3,32,32):expand(TrainData.data:size(1),3,32,32))
+    TestData.data:add(-1, means:view(1,3,32,32):expand(TestData.data:size(1),3,32,32))
+
+    if not loaded then
+        std = torch.std(TrainData.data, 1):squeeze()
+    end
+    TrainData.data:cdiv(std:view(1,3,32,32):expand(TrainData.data:size(1),3,32,32))
+    TestData.data:cdiv(std:view(1,3,32,32):expand(TestData.data:size(1),3,32,32))
+
+    if not loaded then
+        torch.save(meansfile,means)
+        torch.save(stdfile,std)
+    end
+
+end
+
+local TrainDataProvider = DataProvider{
+    Name = 'TrainingData',
+    CachePrefix = nil,
+    CacheFiles = false,
+    Source = {TrainData.data,TrainData.label},
+    MaxNumItems = 1e6,
+    CopyData = false,
+    TensorType = 'torch.FloatTensor',
+
+}
+local TestDataProvider = DataProvider{
+    Name = 'TestData',
+    CachePrefix = nil,
+    CacheFiles = false,
+    Source = {TestData.data, TestData.label},
+    MaxNumItems = 1e6,
+    CopyData = false,
+    TensorType = 'torch.FloatTensor',
+
+}
+
+return{
+    TrainData = TrainDataProvider,
+    TestData = TestDataProvider,
+    Classes = Classes
+}
---
+++ Main.lua
@@ -0,0 +1,209 @@
+require 'torch'
+require 'xlua'
+require 'optim'
+require 'pl'
+require 'eladtools'
+require 'trepl'
+
+----------------------------------------------------------------------
+
+print '==> processing options'
+
+cmd = torch.CmdLine()
+cmd:addTime()
+cmd:text()
+cmd:text('Training a network on CIFAR 10/100')
+cmd:text()
+cmd:text('==>Options')
+
+cmd:text('===>Model And Training Regime')
+cmd:option('-modelsFolder',       './Models/',            'Models Folder')
+cmd:option('-network',            'Model.lua',            'Model file - must return valid network.')
+cmd:option('-LR',                 0.1,                    'learning rate')
+cmd:option('-LRDecay',            0,                      'learning rate decay (in # samples)')
+cmd:option('-weightDecay',        1e-4,                   'L2 penalty on the weights')
+cmd:option('-momentum',           0.9,                    'momentum')
+cmd:option('-batchSize',          128,                    'batch size')
+cmd:option('-optimization',       'sgd',                  'optimization method')
+cmd:option('-epoch',              -1,                     'number of epochs to train, -1 for unbounded')
+
+cmd:text('===>Platform Optimization')
+cmd:option('-threads',            8,                      'number of threads')
+cmd:option('-type',               'cuda',                 'float or cuda')
+cmd:option('-devid',              1,                      'device ID (if using CUDA)')
+
+cmd:text('===>Save/Load Options')
+cmd:option('-load',               '',                  'load existing net weights')
+cmd:option('-save',               os.date():gsub(' ',''), 'save directory')
+
+cmd:text('===>Data Options')
+cmd:option('-dataset',            'Cifar10',              'Dataset - Cifar10 or Cifar100')
+cmd:option('-whiten',             false,                  'whiten data')
+cmd:option('-augment',            false,                  'Augment training data')
+cmd:option('-preProcDir',         './PreProcData/',       'Data for pre-processing (means,P,invP)')
+
+cmd:text('===>Misc')
+cmd:option('-visualize',          1,                      'visualizing results')
+
+opt = cmd:parse(arg or {})
+opt.network = opt.modelsFolder .. paths.basename(opt.network, '.lua')
+opt.save = paths.concat('./Results', opt.save)
+opt.preProcDir = paths.concat(opt.preProcDir, opt.dataset .. '/')
+os.execute('mkdir -p ' .. opt.preProcDir)
+torch.setnumthreads(opt.threads)
+cutorch.setDevice(opt.devid)
+
+torch.setdefaulttensortype('torch.FloatTensor')
+----------------------------------------------------------------------
+-- Model + Loss:
+local model = require(opt.network)
+local loss = nn.ClassNLLCriterion()
+-- classes
+local data = require 'Data'
+local classes      = data.Classes
+
+local ccn2_compatibility = true
+----------------------------------------------------------------------
+
+-- This matrix records the current confusion across classes
+local confusion = optim.ConfusionMatrix(classes)
+
+-- Output files configuration
+os.execute('mkdir -p ' .. opt.save)
+os.execute('cp ' .. opt.network .. '.lua ' .. opt.save)
+cmd:log(opt.save .. '/ConfigLog.log', opt)
+local weights_filename = paths.concat(opt.save, 'Weights.t7')
+local log_filename = paths.concat(opt.save,'TrainingLog.log')
+local Log = optim.Logger(log_filename)
+----------------------------------------------------------------------
+local TensorType = 'torch.FloatTensor'
+if opt.type =='cuda' then
+    model:cuda()
+    loss = loss:cuda()
+    TensorType = 'torch.CudaTensor'
+end
+
+local optimState = {
+    learningRate = opt.LR,
+    momentum = opt.momentum,
+    weightDecay = opt.weightDecay,
+    learningRateDecay = opt.LRDecay
+}
+
+local function updateConfusion(y,yt)
+    confusion:batchAdd(y,yt)
+end
+
+-- Optimization configuration
+local Weights,Gradients = model:getParameters()
+
+if paths.filep(opt.load) then
+    local w = torch.load(opt.load)
+    print('==>Loaded Weights from: ' .. opt.load)
+    Weights:copy(w)
+end
+
+local optimizer = Optimizer{
+    Model = model,
+    Loss = loss,
+    OptFunction = _G.optim[opt.optimization],
+    OptState = optimState,
+    Parameters = {Weights, Gradients},
+    HookFunction = updateConfusion
+}
+
+
+------------------------------
+local function Train(Data)
+
+    model:training()
+
+    local MiniBatch = DataProvider{
+        Name = 'GPU_Batch',
+        MaxNumItems = opt.batchSize,
+        Source = Data,
+        TensorType = TensorType
+    }
+
+    local yt = MiniBatch.Labels
+    local x = MiniBatch.Data
+    local SizeData = Data:size()
+    local NumSamples = 0
+    local NumBatches = 0
+    while MiniBatch:GetNextBatch() do
+        NumSamples = NumSamples+x:size(1)
+        NumBatches = NumBatches + 1
+        if ccn2_compatibility==false or math.fmod(x:size(1),32)==0 then
+            optimizer:optimize(x, yt)
+        end
+        xlua.progress(NumSamples, SizeData)
+
+        if math.fmod(NumBatches,100)==0 then
+            collectgarbage()
+        end
+    end
+
+end
+------------------------------
+local function Test(Data)
+
+    model:evaluate()
+
+    local MiniBatch = DataProvider{
+        Name = 'GPU_Batch',
+        MaxNumItems = opt.batchSize,
+        Source = Data,
+        TensorType = TensorType
+    }
+
+    local yt = MiniBatch.Labels
+    local x = MiniBatch.Data
+    local SizeData = Data:size()
+    local NumSamples = 0
+    local NumBatches = 0
+    while MiniBatch:GetNextBatch() do
+        NumSamples = NumSamples+x:size(1)
+        NumBatches = NumBatches + 1
+        if ccn2_compatibility==false or math.fmod(x:size(1),32)==0 then
+            local y = model:forward(x)
+            updateConfusion(y,yt)
+        end
+        xlua.progress(NumSamples, SizeData)
+
+        if math.fmod(NumBatches,500)==0 then
+           -- image.display(x)
+            collectgarbage()
+        end
+    end
+
+end
+
+local epoch = 1
+while true do
+    data.TrainData:ShuffleItems()
+
+    print('Epoch ' .. epoch) 
+    confusion:zero()
+    Train(data.TrainData)
+    torch.save(weights_filename, Weights)
+    confusion:updateValids()
+    local ErrTrain = (1-confusion.totalValid)
+    if #classes <= 10 then
+        print(confusion)
+    end
+    confusion:zero()
+    Test(data.TestData)
+    confusion:updateValids()
+    local ErrTest = (1-confusion.totalValid)
+    if #classes <= 10 then
+        print(confusion)
+    end
+    print('Training Error = ' .. ErrTrain)
+    print('Test Error = ' .. ErrTest)
+    Log:add{['Training Error']= ErrTrain, ['Test Error'] = ErrTest}
+    if opt.visualize == 1 then
+        Log:style{['Training Error'] = '-', ['Test Error'] = '-'}
+        Log:plot()
+    end
+    epoch = epoch+1
+end
